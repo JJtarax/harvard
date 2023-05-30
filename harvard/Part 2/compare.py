1d2b163b66375219784b2a359b76ea80d7db8e72
JJtarax <93460521+JJtarax@users.noreply.github.com>
2023-05-29 23:26:12 -0400

    Update compare.py

--- harvard/Part 2/compare.py
+++ harvard/Part 2/compare.py
@@ -1,9 +1,16 @@
 x = int(input("What's x? "))
 y= int(input("What's y? "))
 
+if x != y:
+    print("x is not equal to y")
+
+else: # x == y
+    print("x is equal to y")
+'''
 if x < y:
     print("x is less then y")
-if x > y:
+elif x > y:
     print("x is greater than y")
-if x == y:
+elif x == y:
     print("x is equal to y")
+'''
