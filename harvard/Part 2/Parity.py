18e98fa85c38d0979a5e9489d352abca25793d93
JJtarax <93460521+JJtarax@users.noreply.github.com>
2023-06-05 23:20:52 -0400

    part3 done

--- harvard/Part 2/Parity.py
+++ harvard/Part 2/Parity.py
@@ -1,2 +1,13 @@
+<<<<<<< HEAD
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
 
-main()
+
+def is_even (n):
+    return (n % 2 == 0)
+
+main ()
