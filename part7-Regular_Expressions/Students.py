30437a2479ed5452d65b4e99e3fed263211cc266
JJtarax <93460521+JJtarax@users.noreply.github.com>
2023-06-18 15:58:21 -0400

    quick update

--- part7-Regular_Expressions/Students.py
+++ part7-Regular_Expressions/Students.py
@@ -1,4 +1,10 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        name, house = line.rstrip().split(",")
+        student = {"name":name , "house": house}
+        students.append(student)
+
+for students in sorted(students):
+    print(f"{students['name']} is in {students['house']}")
