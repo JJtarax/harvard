f2d09333dc80e07c3bd1401a3a49c3bcf02e0679
JJtarax <93460521+JJtarax@users.noreply.github.com>
2023-06-18 16:15:33 -0400

    fix?

--- part7-Regular_Expressions/Students.py
+++ part7-Regular_Expressions/Students.py
@@ -3,8 +3,12 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        student = {"name":name , "house": house}
-        students.append(student)
+        students.append({"name": name, "house": house})
 
-for students in sorted(students):
-    print(f"{students['name']} is in {students['house']}")
+
+def get_name(student):
+    return student["name"]
+
+
+for student in sorted(students, key=get_name):
+    print(f"{student['name']} is in {student['house']}")
