7adc38a032a5c9e8c3160568ee178b9892e250c8
JJtarax <93460521+JJtarax@users.noreply.github.com>
2023-06-22 07:44:06 -0500

    update

--- part5-Unit_test/test_calculator.py
+++ part5-Unit_test/test_calculator.py
@@ -1,32 +1,22 @@
+import pytest
+
 from calculator import square
 
 
-def main():
-    test_square()
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
 
 
-def test_square():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("2 squared was not 4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 square was not 9")
-    try:
-        assert square(0) == 0
-    except AssertionError:
-        print("0 square was not 0")
-    try:
-        assert square(-2) == 4
-    except AssertionError:
-        print("-2 square was not 4")
-    try:
-        assert square(-3) == 9
-    except AssertionError:
-        print("-3 square was not 9")
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
 
 
-if __name__ == "__main__":
-    main()
+def test_zero():
+    assert square(0) == 0
+
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
--- part8-Regular_Expressions/format.py
+++
@@ -1,7 +0,0 @@
-import re
-
-name = input("What's your name? ").strip()
-match = re.search(r"^.+, .+$", name)
-if match:
-    name = match.group(2) + " " + match.group(1)
-print(f"hello, {name}")
